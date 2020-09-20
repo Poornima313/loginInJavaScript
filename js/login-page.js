53263f9e4d295f39269a1b5d35a14b006a7fb2d4
Poornima313 <poornima31p@gmail.com>
2020-09-20 21:11:23 +0530

    adding collapse functionality

--- js/login-page.js
+++ js/login-page.js
@@ -1,11 +1,7 @@
-function check(form)
-{
- if(form.userid.value == "odessa" && form.pswrd.value == "odessa")
-  {
-    window.open('table.html')
-  }
- else
- {
-   alert("Error Password or Username")
+function check(form) {
+  if (form.userid.value == "odessa" && form.pswrd.value == "odessa") {
+    window.open("table.html");
+  } else {
+    alert("Error Password or Username");
   }
 }
--- js/table.js
+++ js/table.js
@@ -7,9 +7,21 @@ function myFunction() {
   }
 }
 
-jQuery('button').click(function() {
-  jQuery(this).parents('tr')
-      .closest('tbody')
-      .next('.section')
-      .toggle('slow');
+// jQuery("button").click(function () {
+//   jQuery(this)
+//     .parents("tr")
+//     .closest("tbody")
+//     .next(".section")
+//     .toggle("slow")
+//     .addClass("lightblue");
+
+//   // jQuery("tbody").addClass("lightblue");
+// });
+
+// jQuery("table").on("click", "tr.parent .fa-chevron-down", function () {
+//   jQuery(this).closet("tbody").toggleClass("open");
+// });
+
+$("table").on("click", "tr.parent .fa-chevron-down", function () {
+  $(this).closest("tbody").toggleClass("open");
 });
